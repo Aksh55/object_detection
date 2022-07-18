86f77f3ddf4a2d9c3a4a9758a79d0d7e4e739433
Akshaynie Mishra <84663060+Aksh55@users.noreply.github.com>
2022-07-18 19:02:43 +0530

    Add files via upload

---
+++ bedroom.js
@@ -0,0 +1,58 @@
+img ="";
+status = "";
+objects = [];
+function preload()
+{
+    img = loadImage('bedroom.jpg');
+}
+
+function setup()
+{
+    canvas = createCanvas(640, 420);
+    canvas.position(350, 150);
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Object Detected";
+}
+
+function modelLoaded()
+{
+    console.log("Model Loaded!");
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results)
+{
+    if (error)
+    {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+}
+ 
+function draw()
+{
+
+    if(status != "")
+    image(img , 0 , 0 , 640 , 420);
+    {
+        for(var i=0; i< objects.length; i++)
+        {
+            document.getElementById("status").innerHTML = "Status : Detecting Objects";
+            document.getElementById("object_number").innerHTML = "No. of Objects Detected:" + objects.length;
+
+            fill(255,0,0);
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke(255,0,0);
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
+}
+
+function back()
+{
+    window.location = "index.html" ;
+}
---
+++ entrance.js
@@ -0,0 +1,20 @@
+function lr()
+{
+    window.location = "living_room.html"
+}
+function br()
+{
+    window.location = "bedroom.html"
+}
+function h()
+{
+    window.location = "hallway.html"
+}
+function k()
+{
+    window.location = "kitchen.html"
+}
+function wp()
+{
+    window.location = "work_place.html"
+}
